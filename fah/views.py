8a0bd1b314554b9e47707465c4f4c7fdc132c45b
Anton Duris <toniid33@gmail.com>
2024-03-23 17:01:16 +0100

    reverting object based views

---
+++ fah/views.py
@@ -0,0 +1,50 @@
+import json
+from typing import Union
+
+from django.forms import model_to_dict
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from matplotlib.figure import Figure
+
+from fah.boinc import BoincSimulation
+from fah.utils.utils import Utils
+
+sim = BoincSimulation()
+sim.make_net()
+
+
+def plot_stats(request):
+    fig: Figure = sim.plot_stats()
+    uri = Utils.get_image(fig=fig)
+    return render(request=request, template_name='home.html', context={'data': uri})
+
+
+def plot_deadline(request):
+    fig: Figure = sim.plot_deadline()
+    uri = Utils.get_image(fig=fig)
+    return render(request=request, template_name='home.html', context={'data': uri})
+
+
+def plot_pause_deadline(request):
+    fig = sim.plot_pause_deadline()
+    uri = Utils.get_image(fig=fig)
+    return render(request=request, template_name='home.html', context={'data': uri})
+
+
+def plot_gant(request):
+    fig = sim.plot_gant()
+    uri = Utils.get_image(fig=fig)
+    return render(request=request, template_name='home.html', context={'data': uri})
+
+
+def params(request):
+    return JsonResponse(model_to_dict(sim.params), safe=False, json_dumps_params={'indent': 2})
+
+
+@csrf_exempt
+def change_params(request):
+    if request.method == "POST":
+        data: dict[str: Union[int, list[int]]] = json.loads(request.body)
+        sim.params.update(**data)
+        return JsonResponse(data)
